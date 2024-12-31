43f356ddfefc22f32c90700e491ccba00dcefa5b
MohaA1986 <165592243+MohaA1986@users.noreply.github.com>
2024-12-31 20:55:24 +0800

    WH test 3

--- src/test/java/Automation_Abdul/SeleniumFramework/BaseTest.java
+++ src/test/java/Automation_Abdul/SeleniumFramework/BaseTest.java
@@ -6,7 +6,7 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
-//test import2
+//test import3
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
